dac642efaf8cf99152de05bdd24b30515c154f1d
DemonKingSwarn <rockingswarn@gmail.com>
2024-10-14 18:50:11 +0530

    3x+1 visualizer

---
+++ main.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+the_eqn() {
+  if [ $(expr "$num" % 2 ) = 0 ]; then
+    num=$(expr "$num" / 2 )
+  else
+    num=$(expr 3 \* "$num" + 1 )
+  fi
+
+  printf "%s\n" "$num"
+  steps="$steps $num"
+}
+
+printf "%s" "Enter a number: " && read -r num
+
+while [ $num != 1 ]; do
+  the_eqn
+done
+
+printf "%s\n" "Sequence: $steps"
+
+for step in $steps; do
+  echo "$i $step" >> data.txt
+  i=$(expr $i + 1 )
+done
